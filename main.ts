4bd083b888676bd8bbb866721db18a0f9804fc44
zhongyuming <puppetdevz@163.com>
2024-06-17 23:41:47 +0800

    style: change code style.

--- main.ts
+++ main.ts
@@ -1,194 +1,174 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Menu,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
-import Markdownit from "markdown-it";
+import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
+import Markdownit from 'markdown-it'
 
 // Remember to rename these classes and interfaces!
 
 interface ContentStyleManagerPluginSettings {
-	mySetting: string;
-	debugMode: boolean;
+  mySetting: string
+  debugMode: boolean
 }
 
 const DEFAULT_SETTINGS: ContentStyleManagerPluginSettings = {
-	mySetting: "default",
-	debugMode: true,
-};
+  mySetting: 'default',
+  debugMode: true
+}
 
 export default class ContentStyleManagerPlugin extends Plugin {
-	settings: ContentStyleManagerPluginSettings;
+  settings: ContentStyleManagerPluginSettings
 
-	debug() {
-		if (this.settings.debugMode) {
-			const editor =
-				this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
-			// Object.defineProperty(this.app,'editor',{value:editor})
-			// window['editor'] = editor
-			console.log("Add property editor");
-		}
-	}
+  debug() {
+    if (this.settings.debugMode) {
+      const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
+      // Object.defineProperty(this.app,'editor',{value:editor})
+      // window['editor'] = editor
+      console.log('Add property editor')
+    }
+  }
 
-	log(...args: any[]) {
-		if (this.settings.debugMode) {
-			new Notice(args.join(""));
-		}
-	}
+  log(...args: any[]) {
+    if (this.settings.debugMode) {
+      new Notice(args.join(''))
+    }
+  }
 
-	async getCurrentThemeStyles(): Promise<string> {
-		const styles = Array.from(document.styleSheets)
-			.map((styleSheet) => {
-				try {
-					return Array.from(styleSheet.cssRules)
-						.map((rule) => rule.cssText)
-						.join("");
-				} catch (e) {
-					console.error("Could not access stylesheet: ", e);
-					return "";
-				}
-			})
-			.join("");
-		return styles;
-	}
+  async getCurrentThemeStyles(): Promise<string> {
+    const styles = Array.from(document.styleSheets)
+      .map(styleSheet => {
+        try {
+          return Array.from(styleSheet.cssRules)
+            .map(rule => rule.cssText)
+            .join('')
+        } catch (e) {
+          console.error('Could not access stylesheet: ', e)
+          return ''
+        }
+      })
+      .join('')
+    return styles
+  }
 
-	async applyThemeStyles(html: string): Promise<string> {
-		const themeStyles = await this.getCurrentThemeStyles();
-		return `
+  async applyThemeStyles(html: string): Promise<string> {
+    const themeStyles = await this.getCurrentThemeStyles()
+    return `
 		  <html>
 		  <head>
 			<style>${themeStyles}</style>
 		  </head>
 		  <body>${html}</body>
 		  </html>
-		`;
-	}
+		`
+  }
 
-	convertMarkdownToHtml(markdown: string): string {
-		// 使用markdown-it将Markdown转换为HTML
-		const md = new Markdownit();
-		return md.render(markdown);
-	}
+  convertMarkdownToHtml(markdown: string): string {
+    // 使用markdown-it将Markdown转换为HTML
+    const md = new Markdownit()
+    return md.render(markdown)
+  }
 
-	async copyToClipboard(text: string) {
-		try {
-			await navigator.clipboard.writeText(text);
-		} catch (error) {
-			this.log("Failed to copy text: ", error);
-		}
-	}
+  async copyToClipboard(text: string) {
+    try {
+      await navigator.clipboard.writeText(text)
+    } catch (error) {
+      this.log('Failed to copy text: ', error)
+    }
+  }
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // This creates an icon in the left ribbon.
+    // const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+    // Called when the user clicks the icon.
+    // 	new Notice('This is a notice!');
+    // });
+    // Perform additional things with the ribbon
+    // ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    // const statusBarItemEl = this.addStatusBarItem();
+    // statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// If checking is true, we're simply "checking" if the command can be run.
-		// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
+    // This adds a simple command that can be triggered anywhere
+    // this.addCommand({
+    // 	id: 'open-sample-modal-simple',
+    // 	name: 'Open sample modal (simple)',
+    // 	callback: () => {
+    // 		new SampleModal(this.app).open();
+    // 	}
+    // });
+    // This adds an editor command that can perform some operation on the current editor instance
+    // this.addCommand({
+    // 	id: 'sample-editor-command',
+    // 	name: 'Sample editor command',
+    // 	editorCallback: (editor: Editor, view: MarkdownView) => {
+    // 		console.log(editor.getSelection());
+    // 		editor.replaceSelection('Sample Editor Command');
+    // 	}
+    // });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    // this.addCommand({
+    // 	id: 'open-sample-modal-complex',
+    // 	name: 'Open sample modal (complex)',
+    // 	checkCallback: (checking: boolean) => {
+    // Conditions to check
+    // 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    // 		if (markdownView) {
+    // If checking is true, we're simply "checking" if the command can be run.
+    // If checking is false, then we want to actually perform the operation.
+    // 			if (!checking) {
+    // 				new SampleModal(this.app).open();
+    // 			}
 
-		// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
+    // This command will only show up in Command Palette when the check function returns true
+    // 			return true;
+    // 		}
+    // 	}
+    // });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    // this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+    // 	console.log('click', evt);
+    // });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 
-		this.registerEvent(
-			this.app.workspace.on(
-				"editor-menu",
-				(menu: Menu, editor: Editor, view: MarkdownView) => {
-					menu.addItem((item) => {
-						item.setTitle("Copy with Style")
-							.setIcon("copy")
-							.onClick(async () => {
-								const selectedText = editor.getSelection();
-								// this.log('Selected text:', selectedText);
-								if (selectedText) {
-									const htmlContent =
-										this.convertMarkdownToHtml(
-											selectedText
-										);
-									// this.log('HTML content:', htmlContent);
-									await this.copyToClipboard(htmlContent);
-									new Notice("Copied with style!");
-								} else {
-									new Notice("No text selected.");
-								}
-							});
-					});
-				}
-			)
-		);
-	}
+    this.registerEvent(
+      this.app.workspace.on('editor-menu', (menu: Menu, editor: Editor, view: MarkdownView) => {
+        menu.addItem(item => {
+          item
+            .setTitle('Copy with Style')
+            .setIcon('copy')
+            .onClick(async () => {
+              const selectedText = editor.getSelection()
+              // this.log('Selected text:', selectedText);
+              if (selectedText) {
+                const htmlContent = this.convertMarkdownToHtml(selectedText)
+                // this.log('HTML content:', htmlContent);
+                await this.copyToClipboard(htmlContent)
+                new Notice('Copied with style!')
+              } else {
+                new Notice('No text selected.')
+              }
+            })
+        })
+      })
+    )
+  }
 
-	onunload() {}
+  onunload() {}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 }
 
 // class SampleModal extends Modal {
